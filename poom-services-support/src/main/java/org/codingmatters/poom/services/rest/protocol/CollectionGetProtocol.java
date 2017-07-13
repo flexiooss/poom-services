613956adbd6579aa40a32324c78f990b36d530c8
nelt <nel.taurisson@gmail.com>
2017-07-13 04:48:07 +0000

    [poom services] introduced CollectionGetProtocol interface with default implementation of REST get collection and implementation guidance

---
+++ poom-services-support/src/main/java/org/codingmatters/poom/services/rest/protocol/CollectionGetProtocol.java
@@ -0,0 +1,67 @@
+package org.codingmatters.poom.services.rest.protocol;
+
+import org.codingmatters.poom.services.domain.exceptions.RepositoryException;
+import org.codingmatters.poom.services.domain.repositories.Repository;
+import org.codingmatters.poom.services.support.logging.LoggingContext;
+import org.codingmatters.poom.services.support.paging.Rfc7233Pager;
+import org.slf4j.MDC;
+
+import java.util.UUID;
+import java.util.function.Function;
+
+/**
+ * Created by nelt on 7/13/17.
+ */
+public interface CollectionGetProtocol<V, Q, Req, Resp> extends Function<Req, Resp> {
+
+    Repository<V, Q> repository();
+
+    int maxPageSize();
+
+    String rfc7233Unit();
+    String rfc7233Range(Req request);
+
+    Q parseQuery(Req request);
+
+    Resp partialJobList(Rfc7233Pager.Page page);
+    Resp completeList(Rfc7233Pager.Page page);
+    Resp invalidRangeQuery(Rfc7233Pager.Page page, String errorToken);
+    Resp unexpectedError(RepositoryException e, String errorToken);
+
+    default Resp apply(Req request) {
+        try(LoggingContext ctx = LoggingContext.start()) {
+            MDC.put("request-id", UUID.randomUUID().toString());
+            try {
+                Rfc7233Pager<V, Q> pager = Rfc7233Pager.forRequestedRange(this.rfc7233Range(request))
+                        .unit(this.rfc7233Unit())
+                        .maxPageSize(this.maxPageSize())
+                        .pager(this.repository());
+
+                Q query = this.parseQuery(request);
+
+                Rfc7233Pager.Page page;
+                if(query != null) {
+                    page = pager.page(query);
+                } else {
+                    page = pager.page();
+                }
+
+                if(page.isValid()) {
+                    if (page.isPartial()) {
+                        return this.partialJobList(page);
+                    } else {
+                        return this.completeList(page);
+                    }
+                } else {
+                    String errorToken = UUID.randomUUID().toString();
+                    MDC.put("error-token", errorToken);
+                    return this.invalidRangeQuery(page, errorToken);
+                }
+            } catch (RepositoryException e) {
+                String errorToken = UUID.randomUUID().toString();
+                MDC.put("error-token", errorToken);
+                return this.unexpectedError(e, errorToken);
+            }
+        }
+    }
+}
