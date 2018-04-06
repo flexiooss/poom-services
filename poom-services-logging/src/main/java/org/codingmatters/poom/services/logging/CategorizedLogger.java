2164e12d63f9a37f7f72efa8b351a19531ef11e4
Nel <nel@flexio.fr>
2018-04-06 15:03:20 +0200

    category logging : remover SLF4J factory

--- poom-services-logging/src/main/java/org/codingmatters/poom/services/logging/CategorizedLogger.java
+++ poom-services-logging/src/main/java/org/codingmatters/poom/services/logging/CategorizedLogger.java
@@ -1,14 +1,10 @@
 package org.codingmatters.poom.services.logging;
 
 import org.codingmatters.poom.services.logging.marked.MarkedCategorizedLogger;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public interface CategorizedLogger extends Log {
 
-    static CategorizedLogger getLogger(Logger logger) {
-        return new MarkedCategorizedLogger(logger);
-    }
     static CategorizedLogger getLogger(Class<?> clazz) {
         return new MarkedCategorizedLogger(LoggerFactory.getLogger(clazz));
     }
