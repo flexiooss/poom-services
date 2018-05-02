144172424f3b80abfb87e94f25cf4b676a99473c
Nel <nel@flexio.fr>
2018-05-02 06:07:47 +0200

    oom-ci poped out a own repository

--- poom-ci/poom-ci-ciphering/src/main/java/org/codingmatters/poom/ci/ciphering/DataCiphering.java
+++
@@ -1,9 +0,0 @@
-package org.codingmatters.poom.ci.ciphering;
-
-public interface DataCiphering {
-
-    String DATA_TRANSFORMATION = "AES/CBC/PKCS5Padding";
-    String TRANSPORT_KEY_ALGORITHM = "AES";
-    String TRANSPORT_KEY_TRANSFORMATION = "RSA";
-
-}
--- poom-ci/poom-ci-runners/src/test/java/org/codingmatters/poom/ci/runners/PoomCIRunnerTest.java
+++
@@ -1,9 +0,0 @@
-package org.codingmatters.poom.ci.runners;
-
-public class PoomCIRunnerTest {
-
-    public static void main(String[] args) {
-        PoomCIRunner.main(args);
-    }
-
-}
