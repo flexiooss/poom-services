a7f5f2944b7a3616df5d8973eebe91df53271a4f
Nel <nel@flexio.fr>
2018-05-28 14:20:59 +0200

    test categories definition

---
+++ poom-services-test-support/src/main/java/org/codingmatters/poom/services/test/support/test/categories/TestCategories.java
@@ -0,0 +1,7 @@
+package org.codingmatters.poom.services.test.support.test.categories;
+
+public interface TestCategories {
+    interface LocalOnly {}
+    interface Integration {}
+    interface VerySlow {}
+}
