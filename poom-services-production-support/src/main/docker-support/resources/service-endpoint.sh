db1c9427799db3f72ed75d51436cf21d4382a21b
Nel <nel@flexio.fr>
2018-05-31 16:11:13 +0200

    gelf logging and prod script

--- poom-services-production-support/src/main/docker-support/resources/service-endpoint.sh
+++ poom-services-production-support/src/main/docker-support/resources/service-endpoint.sh
@@ -7,9 +7,15 @@ if [ -z ${LOGGING_TYPE} ]
 then
     LOGGING_TYPE="text"
 fi
-echo "setting logging type to ${LOGGING_TYPE}"
+
+echo "setting logging type to ${LOGGING_TYPE} with level ${LOG_LEVEL}"
 
 LOGGER_CONFIG="-Dlogback.configurationFile=/var/service/config/logs/logback-${LOGGING_TYPE}.xml"
+if [ -z ${LOG_LEVEL} ]
+then
+    LOGGER_CONFIG="${LOGGER_CONFIG} -Droot.logger.level=${LOG_LEVEL}"
+fi
+
 CLASSPATH="/var/service/lib/*:/var/service/config/"
 
 exec java -cp $CLASSPATH $LOGGER_CONFIG $JVM_VM "$@"
