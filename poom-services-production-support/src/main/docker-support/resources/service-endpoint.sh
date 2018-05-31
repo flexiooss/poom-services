14f140798b43bf49cd6767456503bd83577157ca
Nel <nel@flexio.fr>
2018-05-31 12:40:58 +0200

    gelf logging and prod script

--- poom-services-production-support/src/main/docker-support/resources/service-endpoint.sh
+++ poom-services-production-support/src/main/docker-support/resources/service-endpoint.sh
@@ -2,5 +2,12 @@
 
 echo "starting service..."
 export SERVICE_HOST=$(hostname -i)
-exec java -cp "/var/service/lib/*:/var/service/config/" $JVM_VM "$@"
+
+if [ -z ${LOGGING_TYPE} ]
+then
+    LOGGING_TYPE="text"
+fi
+
+echo "setting logging type to ${LOGGING_TYPE}"
+exec java -cp "/var/service/lib/*:/var/service/config/logs/logback-${LOGGING_TYPE}.xml:/var/service/config/" $JVM_VM "$@"
 echo "service stopped"
