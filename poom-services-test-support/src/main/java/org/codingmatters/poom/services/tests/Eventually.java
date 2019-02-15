5e1125550ecc51ecd80bd0e77ecf2f82575d82ef
nelt <nel@flexio.fr>
2019-02-15 06:21:41 +0100

    changed Eventually signatures

--- poom-services-test-support/src/main/java/org/codingmatters/poom/services/tests/Eventually.java
+++ poom-services-test-support/src/main/java/org/codingmatters/poom/services/tests/Eventually.java
@@ -3,22 +3,39 @@ package org.codingmatters.poom.services.tests;
 import org.hamcrest.Matcher;
 import org.hamcrest.MatcherAssert;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Eventually {
 
-    static private AtomicLong timeout = new AtomicLong(2 * 1000L);
-
-    public static void timeout(long t) {
-        timeout.set(t);
+    public static Eventually defaults() {
+        return timeout(2 * 1000L);
     }
 
-    public static <T> void assertThat(CheckedSupplier<T> actual, Matcher<? super T> matcher) {
+    public static Eventually timeout(long t) {
+        return timeout(t, TimeUnit.MILLISECONDS);
+    }
+
+    public static Eventually timeout(long t, TimeUnit tu) {
+        return new Eventually(tu.toMillis(t));
+    }
+
+    @FunctionalInterface
+    public interface CheckedSupplier<T> {
+        T get() throws Exception;
+    }
+
+    private final long timeout;
+
+    public Eventually(long withTimmeout) {
+        this.timeout = withTimmeout;
+    }
+
+    public <T> void assertThat(CheckedSupplier<T> actual, Matcher<? super T> matcher) {
         assertThat("", actual, matcher);
     }
 
-    public static <T> void assertThat(String reason, CheckedSupplier<T> actual,
-                                      Matcher<? super T> matcher) {
+    public <T> void assertThat(String reason, CheckedSupplier<T> actual, Matcher<? super T> matcher) {
         long start = System.currentTimeMillis();
         AssertionError last = null;
         do {
@@ -30,13 +47,9 @@ public class Eventually {
             } catch (Exception e) {
                 last = new AssertionError("failed invoking actual value supplier", e);
             }
-        } while (System.currentTimeMillis() - start < timeout.get());
+        } while (System.currentTimeMillis() - start < this.timeout);
 
         throw last;
     }
 
-    @FunctionalInterface
-    public interface CheckedSupplier<T> {
-        T get() throws Exception;
-    }
 }
