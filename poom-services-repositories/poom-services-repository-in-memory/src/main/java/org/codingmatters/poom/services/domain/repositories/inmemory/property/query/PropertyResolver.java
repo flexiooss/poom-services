c65351648d0325d8a3a712224c8d0cd421ba9da5
nelt <nel@flexio.fr>
2019-11-15 11:50:46 +0100

    done
    ref #17

--- poom-services-repositories/poom-services-repository-in-memory/src/main/java/org/codingmatters/poom/services/domain/repositories/inmemory/property/query/PropertyResolver.java
+++ poom-services-repositories/poom-services-repository-in-memory/src/main/java/org/codingmatters/poom/services/domain/repositories/inmemory/property/query/PropertyResolver.java
@@ -1,6 +1,8 @@
 package org.codingmatters.poom.services.domain.repositories.inmemory.property.query;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Objects;
 
 public class PropertyResolver {
     private final Class valueObjectCalss;
@@ -10,6 +12,9 @@ public class PropertyResolver {
     }
 
     public boolean hasProperty(String property) {
+        if(this.isNestedProperty(property)) {
+            return this.hasNestedProperty(this.head(property), this.tail(property));
+        }
         try {
             return this.valueObjectCalss.getMethod(property) != null;
         } catch (NoSuchMethodException e) {
@@ -17,11 +22,49 @@ public class PropertyResolver {
         }
     }
 
+    public String firstPart(String property) {
+        return property.substring(0, property.indexOf('.'));
+    }
+
+    private boolean hasNestedProperty(String property, String subpath) {
+        try {
+            Method method = this.valueObjectCalss.getMethod(property);
+            return new PropertyResolver(method.getReturnType()).hasProperty(subpath);
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
+    }
+
     public Object resolve(Object o, String property) {
+        if(this.isNestedProperty(property)) {
+            return this.resolveNestedProperty(o, this.head(property), this.tail(property));
+        }
         try {
             return this.valueObjectCalss.getMethod(property).invoke(o);
         } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             return null;
         }
+    }
+
+    private Object resolveNestedProperty(Object o, String property, String subpath) {
+        try {
+            Method method = this.valueObjectCalss.getMethod(property);
+            Object sub = method.invoke(o);
+            return new PropertyResolver(method.getReturnType()).resolve(sub, subpath);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            return null;
+        }
+    }
+
+    private boolean isNestedProperty(String property) {
+        return property.indexOf('.') != -1;
+    }
+
+    private String head(String property) {
+        return property.substring(0, property.indexOf('.'));
+    }
+
+    private String tail(String property) {
+        return property.substring(property.indexOf('.') + 1);
     }
 }
--- poom-services-repositories/poom-services-repository-in-memory/src/test/java/org/codingmatters/poom/services/domain/repositories/inmemory/InMemoryRepositoryWithPropertyQueryTest.java
+++ poom-services-repositories/poom-services-repository-in-memory/src/test/java/org/codingmatters/poom/services/domain/repositories/inmemory/InMemoryRepositoryWithPropertyQueryTest.java
@@ -3,6 +3,7 @@ package org.codingmatters.poom.services.domain.repositories.inmemory;
 import org.codingmatters.poom.services.domain.property.query.PropertyQuery;
 import org.codingmatters.poom.services.domain.repositories.Repository;
 import org.codingmatters.test.Simple;
+import org.codingmatters.test.simple.E;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,13 +18,18 @@ public class InMemoryRepositoryWithPropertyQueryTest {
     @Before
     public void setUp() throws Exception {
         for (long i = 0; i < 1000; i++) {
-            this.repository.create(Simple.builder().a(i).build());
+            this.repository.create(Simple.builder().a(i).e(E.builder().f("" + i).build()).build());
         }
     }
 
     @Test
     public void search() throws Exception {
         assertThat(this.repository.search(PropertyQuery.builder().filter("a >= 500").build(), 0, 1000).total(), is(500L));
+    }
+
+    @Test
+    public void searchNested() throws Exception {
+        assertThat(this.repository.search(PropertyQuery.builder().filter("e.f starts with 50").build(), 0, 1000).total(), is(11L));
     }
 
     @Test
