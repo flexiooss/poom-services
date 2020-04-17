149f2a67d50c179c2088a33fe4ad560fcf4ffc9e
nelt <nel@flexio.fr>
2020-04-17 13:37:42 +0200

    import

--- poom-services-repositories/poom-services-repository-in-memory/src/test/java/org/codingmatters/poom/services/domain/repositories/inmemory/InMemoryRepositoryWithPropertyQueryTest.java
+++ poom-services-repositories/poom-services-repository-in-memory/src/test/java/org/codingmatters/poom/services/domain/repositories/inmemory/InMemoryRepositoryWithPropertyQueryTest.java
@@ -8,12 +8,11 @@ import org.codingmatters.test.simple.E;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.MatcherAssert.*;
 
 public class InMemoryRepositoryWithPropertyQueryTest {
 
