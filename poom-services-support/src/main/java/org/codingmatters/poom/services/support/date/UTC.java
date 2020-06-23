1e42546f642b39b9b4cf3beefa6cce404a491dc7
nelt <nel@flexio.fr>
2020-06-23 16:24:20 +0200

    done
    ref #62

--- poom-services-support/src/main/java/org/codingmatters/poom/services/support/date/UTC.java
+++ poom-services-support/src/main/java/org/codingmatters/poom/services/support/date/UTC.java
@@ -2,11 +2,22 @@ package org.codingmatters.poom.services.support.date;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
 
 public class UTC {
 
     static public LocalDateTime now() {
         return LocalDateTime.now(ZoneOffset.UTC.normalized());
+    }
+
+    static public LocalDateTime from(LocalDateTime now, TimeZone tz) {
+        ZonedDateTime atZone = now.atZone(tz.toZoneId());
+        return atZone.withZoneSameInstant(ZoneOffset.UTC.normalized()).toLocalDateTime();
+    }
+
+    static public LocalDateTime from(ZonedDateTime zoned) {
+        return zoned.withZoneSameInstant(ZoneOffset.UTC.normalized()).toLocalDateTime();
     }
 
 }
