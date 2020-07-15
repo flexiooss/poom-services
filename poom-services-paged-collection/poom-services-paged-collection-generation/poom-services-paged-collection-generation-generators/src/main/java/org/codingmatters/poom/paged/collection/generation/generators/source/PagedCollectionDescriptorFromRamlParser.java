37a866991e88a90c30237a44a23c70c073c31605
nelt <nel@flexio.fr>
2020-07-15 12:09:18 +0200

    parsing complete collectiondescriptors
    ref #66

--- poom-services-paged-collection/poom-services-paged-collection-generation/poom-services-paged-collection-generation-generators/src/main/java/org/codingmatters/poom/paged/collection/generation/generators/source/PagedCollectionDescriptorFromRamlParser.java
+++ poom-services-paged-collection/poom-services-paged-collection-generation/poom-services-paged-collection-generation-generators/src/main/java/org/codingmatters/poom/paged/collection/generation/generators/source/PagedCollectionDescriptorFromRamlParser.java
@@ -1,16 +1,132 @@
 package org.codingmatters.poom.paged.collection.generation.generators.source;
 
+import org.codingmatters.poom.paged.collection.generation.spec.Action;
 import org.codingmatters.poom.paged.collection.generation.spec.PagedCollectionDescriptor;
+import org.codingmatters.poom.paged.collection.generation.spec.pagedcollectiondescriptor.Types;
+import org.codingmatters.rest.api.generator.exception.RamlSpecException;
+import org.codingmatters.rest.api.generator.handlers.HandlersHelper;
+import org.codingmatters.rest.api.generator.utils.BodyTypeResolver;
+import org.codingmatters.value.objects.generation.Naming;
 import org.raml.v2.api.RamlModelResult;
+import org.raml.v2.api.model.v10.bodies.Response;
+import org.raml.v2.api.model.v10.methods.Method;
+import org.raml.v2.api.model.v10.resources.Resource;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 public class PagedCollectionDescriptorFromRamlParser {
     private final RamlModelResult ramlModel;
+    private final Naming naming;
+    private final HandlersHelper handlersHelper;
+    private final String typesPackage;
 
-    public PagedCollectionDescriptorFromRamlParser(RamlModelResult ramlModel) {
+    public PagedCollectionDescriptorFromRamlParser(RamlModelResult ramlModel, String apiPackage, String typesPackage) {
         this.ramlModel = ramlModel;
+        this.typesPackage = typesPackage;
+        this.naming = new Naming();
+        this.handlersHelper = new HandlersHelper(apiPackage, this.naming);
     }
 
-    public PagedCollectionDescriptor parse() {
-        return null;
+    public PagedCollectionDescriptor[] parse() throws RamlSpecException {
+        List<PagedCollectionDescriptor> results = new LinkedList<>();
+        for (Resource collection : this.collectPageCollections(this.ramlModel.getApiV10().resources())) {
+            results.add(this.descriptor(collection));
+        }
+        return results.toArray(new PagedCollectionDescriptor[results.size()]);
+    }
+
+    private List<Resource> collectPageCollections(List<Resource> resources) {
+        List<Resource> collections = new LinkedList<>();
+        for (Resource resource : resources) {
+            if(this.isPagedCollection(resource)) {
+                collections.add(resource);
+            }
+            collections.addAll(this.collectPageCollections(resource.resources()));
+        }
+        return collections;
+    }
+
+    private boolean isPagedCollection(Resource resource) {
+        return resource.annotations().stream().anyMatch(
+                annotationRef -> "paged-collection".equals(annotationRef.annotation().name())
+        );
+    }
+
+    private PagedCollectionDescriptor descriptor(Resource collection) throws RamlSpecException {
+        Resource entity = this.entityResource(collection);
+        PagedCollectionDescriptor.Builder builder = PagedCollectionDescriptor.builder()
+                .name(this.naming.type(collection.displayName().value()))
+                .entityIdParam(this.entityIdParam(collection))
+                .types(this.types(collection))
+                .browse(this.resourceAction(collection, "get"))
+                .create(this.resourceAction(collection, "post"))
+                .retrieve(this.resourceAction(entity, "get"))
+                .replace(this.resourceAction(entity, "put"))
+                .update(this.resourceAction(entity, "patch"))
+                .delete(this.resourceAction(entity, "delete"))
+                ;
+
+        return builder.build();
+    }
+
+    private Action resourceAction(Resource collection, String method) {
+        return Action.builder()
+                .requestValueObject(this.handlersHelper.handlerFunctionType(collection.displayName().value(), method).typeArguments.get(0).toString())
+                .responseValueObject(this.handlersHelper.handlerFunctionType(collection.displayName().value(), method).typeArguments.get(1).toString())
+                .build();
+    }
+
+    private String entityIdParam(Resource collection) throws RamlSpecException {
+        Resource entityResource = this.entityResource(collection);
+        return entityResource.uriParameters().get(entityResource.uriParameters().size() - 1).name();
+    }
+
+    private Resource entityResource(Resource collection) throws RamlSpecException {
+        Optional<Resource> entityResource = collection.resources().stream().filter(resource -> this.isPagedCollectionEntity(resource)).findFirst();
+        if(entityResource.isPresent()) {
+            return entityResource.get();
+        } else {
+            throw new RamlSpecException("must have resource annotated with paged-collection-entity under " + collection.resourcePath());
+        }
+    }
+
+    private Types types(Resource collection) throws RamlSpecException {
+        Types.Builder builder = Types.builder();
+
+        Optional<Method> method = collection.methods().stream().filter(m -> m.method().equals("get")).findFirst();
+        if(method.isPresent()) {
+            builder.entity(new BodyTypeResolver(this.status200Response(method.get()).body().get(0), this.typesPackage).resolve().build().typeRef());
+            builder.error(this.typesPackage + ".Error");
+            builder.message(this.typesPackage + ".Message");
+        }
+
+        method = collection.methods().stream().filter(m -> m.method().equals("post")).findFirst();
+        if(method.isPresent()) {
+            builder.create(new BodyTypeResolver(method.get().body().get(0), this.typesPackage).resolve().build().typeRef());
+        }
+
+        Resource entityResource = this.entityResource(collection);
+        method = entityResource.methods().stream().filter(m -> m.method().equals("put")).findFirst();
+        if(method.isPresent()) {
+            builder.replace(new BodyTypeResolver(method.get().body().get(0), this.typesPackage).resolve().build().typeRef());
+        }
+        method = entityResource.methods().stream().filter(m -> m.method().equals("patch")).findFirst();
+        if(method.isPresent()) {
+            builder.update(new BodyTypeResolver(method.get().body().get(0), this.typesPackage).resolve().build().typeRef());
+        }
+
+        return builder.build();
+    }
+
+    private Response status200Response(Method method) {
+        return method.responses().stream().filter(response -> response.code().value().equals("200")).findAny().get();
+    }
+
+    private boolean isPagedCollectionEntity(Resource resource) {
+        return resource.annotations().stream().anyMatch(
+                annotationRef -> "paged-collection-entity".equals(annotationRef.annotation().name())
+        );
     }
 }
--- poom-services-paged-collection/poom-services-paged-collection-generation/poom-services-paged-collection-generation-generators/src/test/java/org/codingmatters/poom/paged/collection/generation/generators/source/PagedCollectionDescriptorFromRamlParserTest.java
+++ poom-services-paged-collection/poom-services-paged-collection-generation/poom-services-paged-collection-generation-generators/src/test/java/org/codingmatters/poom/paged/collection/generation/generators/source/PagedCollectionDescriptorFromRamlParserTest.java
@@ -1,11 +1,190 @@
 package org.codingmatters.poom.paged.collection.generation.generators.source;
 
-import org.junit.Ignore;
+import org.codingmatters.poom.paged.collection.generation.generators.source.test.TestData;
+import org.codingmatters.poom.paged.collection.generation.spec.Action;
+import org.codingmatters.poom.paged.collection.generation.spec.PagedCollectionDescriptor;
+import org.codingmatters.poom.paged.collection.generation.spec.pagedcollectiondescriptor.Types;
+import org.codingmatters.rest.api.generator.exception.RamlSpecException;
+import org.codingmatters.rest.maven.plugin.raml.RamlFileCollector;
+import org.generated.api.*;
+import org.generated.api.types.*;
+import org.generated.api.types.Error;
+import org.junit.Assert;
 import org.junit.Test;
+import org.raml.v2.api.RamlModelBuilder;
 import org.raml.v2.api.RamlModelResult;
 
-import static org.junit.Assert.*;
-@Ignore
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.is;
+
 public class PagedCollectionDescriptorFromRamlParserTest {
 
+    static RamlModelResult TEST_API_RAML = raml("test-api-spec.raml");
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenCollectionNameSetFromCollectionDisplayName() throws Exception {
+        PagedCollectionDescriptor[] parsed = new PagedCollectionDescriptorFromRamlParser(TEST_API_RAML, "org.generated.api", "org.generated.api.types").parse();
+
+        Assert.assertTrue(Arrays.stream(parsed).anyMatch(descriptor -> descriptor.name().equals(TestData.FULL_COLLECTION.name())));
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenTypesAreGetFromApiSpec() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseFullCollection();
+
+        assertThat(
+                collectionDescriptor.types(),
+                is(TestData.FULL_COLLECTION.types())
+        );
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenEntityIdParamIsGetFromPagedCollectionEntityUriParameter() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseFullCollection();
+
+        assertThat(
+                collectionDescriptor.entityIdParam(),
+                is("entity-id")
+        );
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenBrowseActionIsGetFromApi() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseFullCollection();
+
+        assertThat(
+                collectionDescriptor.browse(),
+                is(TestData.FULL_COLLECTION.browse())
+        );
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenRetrieveActionIsGetFromApi() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseFullCollection();
+
+        assertThat(
+                collectionDescriptor.retrieve(),
+                is(TestData.FULL_COLLECTION.retrieve())
+        );
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenCreateActionIsGetFromApi() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseFullCollection();
+
+        assertThat(
+                collectionDescriptor.create(),
+                is(TestData.FULL_COLLECTION.create())
+        );
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenDeleteActionIsGetFromApi() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseFullCollection();
+
+        assertThat(
+                collectionDescriptor.delete(),
+                is(TestData.FULL_COLLECTION.delete())
+        );
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenReplaceActionIsGetFromApi() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseFullCollection();
+
+        assertThat(
+                collectionDescriptor.replace(),
+                is(TestData.FULL_COLLECTION.replace())
+        );
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenUpdateActionIsGetFromApi() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseFullCollection();
+
+        assertThat(
+                collectionDescriptor.update(),
+                is(TestData.FULL_COLLECTION.update())
+        );
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingFullCollection__thenDescriptorIsCreatedFromApi() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseFullCollection();
+
+        assertThat(
+                collectionDescriptor,
+                is(TestData.FULL_COLLECTION)
+        );
+    }
+
+    @Test
+    public void givenParsingTestApi__whenGettingCompleteCollectionWithParams__thenDescriptorIsCreatedFromApi() throws Exception {
+        PagedCollectionDescriptor collectionDescriptor = this.parseCollection("WithParam");
+
+        assertThat(
+                collectionDescriptor,
+                is(PagedCollectionDescriptor.builder()
+                            .name("WithParam").entityIdParam("entity-id")
+                            .types(Types.builder()
+                                    .entity(org.generated.api.types.Entity.class.getName())
+                                    .create(Create.class.getName())
+                                    .replace(Replace.class.getName())
+                                    .update(Update.class.getName())
+                                    .error(Error.class.getName())
+                                    .message(Message.class.getName())
+                                    .build())
+                            .browse(Action.builder()
+                                    .requestValueObject(WithParamGetRequest.class.getName())
+                                    .responseValueObject(WithParamGetResponse.class.getName())
+                                    .build())
+                            .create(Action.builder()
+                                    .requestValueObject(WithParamPostRequest.class.getName())
+                                    .responseValueObject(WithParamPostResponse.class.getName())
+                                    .build())
+                            .retrieve(Action.builder()
+                                    .requestValueObject(WithParamsElementGetRequest.class.getName())
+                                    .responseValueObject(WithParamsElementGetResponse.class.getName())
+                                    .build())
+                            .delete(Action.builder()
+                                    .requestValueObject(WithParamsElementDeleteRequest.class.getName())
+                                    .responseValueObject(WithParamsElementDeleteResponse.class.getName())
+                                    .build())
+                            .replace(Action.builder()
+                                    .requestValueObject(WithParamsElementPutRequest.class.getName())
+                                    .responseValueObject(WithParamsElementPutResponse.class.getName())
+                                    .build())
+                            .update(Action.builder()
+                                    .requestValueObject(WithParamsElementPatchRequest.class.getName())
+                                    .responseValueObject(WithParamsElementPatchResponse.class.getName())
+                                    .build())
+                            .build())
+        );
+    }
+
+    private PagedCollectionDescriptor parseFullCollection() throws Exception {
+        String collectionName = TestData.FULL_COLLECTION.name();
+        return parseCollection(collectionName);
+    }
+
+    private PagedCollectionDescriptor parseCollection(String collectionName) throws RamlSpecException {
+        return Arrays.stream(
+                    new PagedCollectionDescriptorFromRamlParser(TEST_API_RAML, "org.generated.api", "org.generated.api.types").parse()
+            ).filter(pagedCollectionDescriptor -> pagedCollectionDescriptor.name().equals(collectionName)).findFirst().get();
+    }
+
+    static private RamlModelResult raml(String resource) {
+        RamlFileCollector.Builder builder = RamlFileCollector.spec(resource);
+        RamlFileCollector collector;
+        try {
+            collector = builder.build();
+        } catch (IOException e) {
+            throw new RuntimeException("failed parsing " + resource, e);
+        }
+        return new RamlModelBuilder().buildApi(collector.specFile());
+    }
 }
