1aacaaa2d2258e7c7ecf9083f56d87dcfa751202
Thibaud <thibaud@flexio.fr>
2025-01-14 14:55:43 +0100

    Fix message leak in description (NotFoundException, BadRequestException)

--- poom-services-paged-collection/poom-services-paged-collection-domain/src/main/java/org/codingmatters/poom/generic/resource/domain/exceptions/BadRequestException.java
+++ poom-services-paged-collection/poom-services-paged-collection-domain/src/main/java/org/codingmatters/poom/generic/resource/domain/exceptions/BadRequestException.java
@@ -7,12 +7,20 @@ import org.codingmatters.poom.services.logging.CategorizedLogger;
 public class BadRequestException extends Exception {
     private final Error error;
 
+    public BadRequestException(CategorizedLogger log, String message, String description) {
+        this(Error.builder().code(Error.Code.BAD_REQUEST).token(log.tokenized().error(message)).description(description).build(), message);
+    }
+
+    public BadRequestException(CategorizedLogger log, String message, String description, Throwable cause) {
+        this(Error.builder().code(Error.Code.BAD_REQUEST).token(log.tokenized().error(message, cause)).description(description).build(), message, cause);
+    }
+
     public BadRequestException(CategorizedLogger log, String message) {
-        this(Error.builder().code(Error.Code.BAD_REQUEST).token(log.tokenized().error(message)).description(message).build(), message);
+        this(log, message, (String) null);
     }
 
     public BadRequestException(CategorizedLogger log, String message, Throwable cause) {
-        this(Error.builder().code(Error.Code.BAD_REQUEST).token(log.tokenized().error(message, cause)).description(message).build(), message, cause);
+        this(log, message, null, cause);
     }
 
     public BadRequestException(Error error, String message) {
