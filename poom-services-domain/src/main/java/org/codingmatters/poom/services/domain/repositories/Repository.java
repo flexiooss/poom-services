36382ed033907243fd7940e5e258ef3fb2c47c80
nel <nel@flexio.fr>
2025-08-08 15:53:15 +0200

    [repository] createMany + naive default implementation

--- poom-services-domain/src/main/java/org/codingmatters/poom/services/domain/repositories/Repository.java
+++ poom-services-domain/src/main/java/org/codingmatters/poom/services/domain/repositories/Repository.java
@@ -4,6 +4,8 @@ import org.codingmatters.poom.services.domain.exceptions.RepositoryException;
 import org.codingmatters.poom.services.domain.entities.Entity;
 
 import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by nelt on 6/2/17.
@@ -11,7 +13,7 @@ import java.math.BigInteger;
 public interface Repository<V, Q> extends EntityLister<V, Q> {
     Entity<V> create(V withValue) throws RepositoryException;
     Entity<V> retrieve(String id) throws RepositoryException;
-    Entity<V>  update(Entity<V> entity, V withValue) throws RepositoryException;
+    Entity<V> update(Entity<V> entity, V withValue) throws RepositoryException;
     void delete(Entity<V> entity) throws RepositoryException;
 
     default void deleteFrom(Q query) throws RepositoryException {
@@ -24,5 +26,16 @@ public interface Repository<V, Q> extends EntityLister<V, Q> {
 
     default Entity<V> createWithIdAndVersion(String id, BigInteger version, V withValue) throws RepositoryException {
         throw new UnsupportedOperationException();
+    }
+
+    default List<String> createMany(V ... values) throws RepositoryException {
+        List<String> result = new LinkedList<>();
+        if(values != null) {
+            for (V value : values) {
+                Entity<V> created = this.create(value);
+                result.add(created.id());
+            }
+        }
+        return result;
     }
 }
