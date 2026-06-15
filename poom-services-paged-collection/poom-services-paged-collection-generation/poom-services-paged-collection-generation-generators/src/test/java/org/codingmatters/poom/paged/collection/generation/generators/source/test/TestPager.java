3036c72477b9295bbfe1ffc4d2dcf11953444801
nel <nel@flexio.fr>
2026-06-15 17:23:38 +0200

    test: update TestPager to support OrderedLister

--- poom-services-paged-collection/poom-services-paged-collection-generation/poom-services-paged-collection-generation-generators/src/test/java/org/codingmatters/poom/paged/collection/generation/generators/source/test/TestPager.java
+++ poom-services-paged-collection/poom-services-paged-collection-generation/poom-services-paged-collection-generation-generators/src/test/java/org/codingmatters/poom/paged/collection/generation/generators/source/test/TestPager.java
@@ -10,34 +10,32 @@ public class TestPager implements PagedCollectionAdapter.Pager<Entity> {
     private final EntityLister<Entity, PropertyQuery> lister;
     private final int maxPageSize;
     private final int defaultPageSize;
+    private final PagedCollectionAdapter.OrderedLister<Entity> orderedLister;
 
     public TestPager(String unit, EntityLister<Entity, PropertyQuery> lister, int maxPageSize) {
-        this(unit, lister, maxPageSize, maxPageSize);
+        this(unit, lister, maxPageSize, maxPageSize, null);
     }
+
     public TestPager(String unit, EntityLister<Entity, PropertyQuery> lister, int maxPageSize, int defaultPageSize) {
+        this(unit, lister, maxPageSize, defaultPageSize, null);
+    }
+
+    public TestPager(String unit, EntityLister<Entity, PropertyQuery> lister, int maxPageSize,
+                     int defaultPageSize, PagedCollectionAdapter.OrderedLister<Entity> orderedLister) {
         this.unit = unit;
         this.lister = lister;
         this.maxPageSize = maxPageSize;
         this.defaultPageSize = defaultPageSize;
+        this.orderedLister = orderedLister;
     }
 
-    @Override
-    public String unit() {
-        return this.unit;
-    }
+    @Override public String unit() { return this.unit; }
+    @Override public int maxPageSize() { return this.maxPageSize; }
+    @Override public int defaultPageSize() { return this.defaultPageSize; }
+    @Override public EntityLister<Entity, PropertyQuery> lister() { return this.lister; }
 
     @Override
-    public int maxPageSize() {
-        return this.maxPageSize;
-    }
-
-    @Override
-    public int defaultPageSize() {
-        return this.defaultPageSize;
-    }
-
-    @Override
-    public EntityLister<Entity, PropertyQuery> lister() {
-        return this.lister;
+    public PagedCollectionAdapter.OrderedLister<Entity> orderedLister() {
+        return this.orderedLister;
     }
 }
